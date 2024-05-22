282a68523360d02c5c874bbf8e5fb5c1cc899131
undefinedteam1 <77137924+undefinedteam1@users.noreply.github.com>
2024-05-22 14:46:56 +0700

    Update wallet.go

--- wallet.go
+++ wallet.go
@@ -79,37 +79,37 @@ type CoinInfo struct {
 	IsLegalMoney     bool   `json:"isLegalMoney"`
 	Locked           string `json:"locked"`
 	Name             string `json:"name"`
-	NetworkList      []struct {
-		AddressRegex            string `json:"addressRegex"`
-		Coin                    string `json:"coin"`
-		DepositDesc             string `json:"depositDesc"`
-		DepositEnable           bool   `json:"depositEnable"`
-		IsDefault               bool   `json:"isDefault"`
-		MemoRegex               string `json:"memoRegex"`
-		MinConfirm              int    `json:"minConfirm"`
-		Name                    string `json:"name"`
-		Network                 string `json:"network"`
-		ResetAddressStatus      bool   `json:"resetAddressStatus"`
-		SpecialTips             string `json:"specialTips"`
-		UnLockConfirm           int    `json:"unLockConfirm"`
-		WithdrawDesc            string `json:"withdrawDesc"`
-		WithdrawEnable          bool   `json:"withdrawEnable"`
-		WithdrawFee             string `json:"withdrawFee"`
-		WithdrawIntegerMultiple string `json:"withdrawIntegerMultiple"`
-		WithdrawMax             string `json:"withdrawMax"`
-		WithdrawMin             string `json:"withdrawMin"`
-		SameAddress             bool   `json:"sameAddress"`
-		EstimatedArrivalTime    uint64 `json:"estimatedArrivalTime"`
-		Busy                    bool   `json:"busy"`
-		ContractAddressUrl	string `json:"contractAddressUrl"`
-		ContractAddress		string `json:"contractAddress"`
-	} `json:"networkList"`
+	NetworkList      []CoinNetwork `json:"networkList"`
 	Storage           string `json:"storage"`
 	Trading           bool   `json:"trading"`
 	WithdrawAllEnable bool   `json:"withdrawAllEnable"`
 	Withdrawing       string `json:"withdrawing"`
 }
-
+type CoinNetwork struct {
+	AddressRegex            string `json:"addressRegex"`
+	Coin                    string `json:"coin"`
+	DepositDesc             string `json:"depositDesc"`
+	DepositEnable           bool   `json:"depositEnable"`
+	IsDefault               bool   `json:"isDefault"`
+	MemoRegex               string `json:"memoRegex"`
+	MinConfirm              int    `json:"minConfirm"`
+	Name                    string `json:"name"`
+	Network                 string `json:"network"`
+	ResetAddressStatus      bool   `json:"resetAddressStatus"`
+	SpecialTips             string `json:"specialTips"`
+	UnLockConfirm           int    `json:"unLockConfirm"`
+	WithdrawDesc            string `json:"withdrawDesc"`
+	WithdrawEnable          bool   `json:"withdrawEnable"`
+	WithdrawFee             string `json:"withdrawFee"`
+	WithdrawIntegerMultiple string `json:"withdrawIntegerMultiple"`
+	WithdrawMax             string `json:"withdrawMax"`
+	WithdrawMin             string `json:"withdrawMin"`
+	SameAddress             bool   `json:"sameAddress"`
+	EstimatedArrivalTime    uint64 `json:"estimatedArrivalTime"`
+	Busy                    bool   `json:"busy"`
+	ContractAddressUrl	string `json:"contractAddressUrl"`
+	ContractAddress		string `json:"contractAddress"`
+}
 // Daily Account Snapshot (USER_DATA)
 const (
 	accountSnapshotEndpoint = "/sapi/v1/accountSnapshot"
